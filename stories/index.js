cd641a4e87f5976df0ee11deab905aa5499ee613
Amila Welihinda <amilajack@gmail.com>
2016-12-04 19:56:47 -0800

    Initil non-working commit

---
+++ stories/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import Button from './Button';
+import Welcome from './Welcome';
+import Home from '../app/components/Home';
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+    <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Home />
+  ));
+
+storiesOf('Moo', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
